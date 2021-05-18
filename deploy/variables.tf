a7ba1e302cfe43e3c358473a3e841fe61a68809f
abhiroy2005 <abhiroy.me@gmail.com>
2021-05-18 08:19:45 +0530

    terraform added

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,22 +1,27 @@
 
 variable "prefix" {
+  type = string
   default = "pxsc-tht"
 }
 
 variable "project" {
+  type = string
   default = "Test Project"
 }
 
 variable "contact" {
+  type = string
   default = "mdmello@phoenixcontact-sb.io"
 }
 
 variable "db_username" {
+  type = string
   description = "Username fro the postgress database"
-  default="tht"
+  default= "tht"
 }
 
 variable "db_password" {
+  type = string
   description = "passowrd for the postgress database"
-  default="tht"
+  default= "tht"
 }
